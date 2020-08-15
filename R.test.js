82822675a8257c95fa01ac6dfd205b4a15d2e874
thescripted <somethingscripted@gmail.com>
2020-08-15 00:59:26 -0500

    added 10 unit tests

--- R.test.js
+++ R.test.js
@@ -7,7 +7,7 @@ describe('Promise test suite', () => {
       updatedarray = [1, 2, 3, 4, 5, 6];
     });
 
-    expect(updatedarray).toStrictEqual(Array.from([1, 2, 3, 4, 5, 6]));
+    expect(updatedarray).toStrictEqual([1, 2, 3, 4, 5, 6]);
   });
 
   it('should call resolution handler when promise is resolved', () => {
@@ -15,7 +15,7 @@ describe('Promise test suite', () => {
     let promise = new R(function (resolve) {
       setTimeout(function () {
         resolve(testing);
-      });
+      }, 100);
     });
 
     promise.then(res => {
@@ -28,7 +28,7 @@ describe('Promise test suite', () => {
     let promise = new R(function (resolve) {
       setTimeout(function () {
         resolve(testing);
-      }, 500);
+      }, 100);
     });
 
     promise.then(res => {
@@ -52,7 +52,7 @@ describe('Promise test suite', () => {
     let promise = new R(function (resolve) {
       setTimeout(function () {
         resolve();
-      }, 500);
+      }, 100);
     });
 
     promise
@@ -66,6 +66,114 @@ describe('Promise test suite', () => {
       .then(function (res) {
         response = res;
         expect(response).toBe(testing);
+        done();
+      });
+  });
+
+  it('calling resolve should have zero effects when run a second time', done => {
+    let promise = new R(function (resolve) {
+      setTimeout(function () {
+        resolve(40);
+        resolve(80);
+      }, 100);
+    });
+
+    promise.then(function (res) {
+      expect(res).toBe(40);
+      done();
+    });
+  });
+
+  it('should call rejection handler when promise is rejected', done => {
+    let testError = 'Error!';
+    let promise = new R(function (resolve, reject) {
+      setTimeout(function () {
+        reject(testError);
+      }, 100);
+    });
+
+    promise.catch(function (error) {
+      expect(error).toBe(testError);
+      done();
+    });
+  });
+
+  it('should pass rejection downstream', done => {
+    let testError = 'Error!';
+    let promise = new R(function (resolve, reject) {
+      setTimeout(function () {
+        reject(testError);
+      }, 100);
+    });
+
+    promise.then(function () {
+      return new Promise(function (resolve) {
+        setTimeout(function () {
+          resolve(500);
+        }, 100);
+      });
+    });
+
+    promise.catch(function (error) {
+      expect(error).toBe(testError);
+      done();
+    });
+  });
+
+  it('should properly handle rejections from promises rejected in a `then` method', done => {
+    let testError = 'error';
+    let promise = new R(function (resolve, reject) {
+      setTimeout(function () {
+        resolve(500);
+      }, 100);
+    });
+
+    promise
+      .then(function () {
+        return new R(function (resolve, reject) {
+          setTimeout(function () {
+            reject(testError);
+          }, 100);
+        });
+      })
+      .catch(function (error) {
+        expect(error).toBe(testError);
+        done();
+      });
+  });
+
+  it('should catch synchronous errors in resolution handlers', done => {
+    let testError = new Error('something is wrong');
+    let promise = new R(function (resolve, reject) {
+      setTimeout(function () {
+        resolve(500);
+      }, 100);
+    });
+    promise
+      .then(function () {
+        throw testError;
+      })
+      .catch(function (value) {
+        expect(value).toBe(testError);
+        done();
+      });
+  });
+
+  it('should catch synchronous errors in execution handlers', done => {
+    let testError = new Error('something is wrong');
+    let promise = new R(function (resolve, reject) {
+      throw testError;
+    });
+    promise
+      .then(function () {
+        return new R(function (resolve) {
+          setTimeout(function () {
+            resolve(50);
+          }, 100);
+        });
+      })
+      .catch(function (value) {
+        expect(value).toBe(testError);
         done();
       });
   });
--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const REJECTED = 2;
 class R {
   _state = PENDING;
   _value;
-  _queue = [];
+  _reason;
+  _resolutionQueue = [];
+  _rejectionQueue = [];
 
   constructor(fn) {
-    fn(this._resolve.bind(this), this._reject.bind(this));
+    try {
+      fn(this._resolve.bind(this), this._reject.bind(this));
+    } catch (error) {
+      this._reject(error);
+    }
   }
 
+  /**
+   * Transitions the state from PENDING TO RESOLVE, assigns `_value`, and  runs the resolution handler across the resolution queue.
+   *
+   * @param {Any} value
+   */
   _resolve(value) {
     if (this._state !== PENDING) {
       return;
@@ -18,72 +29,131 @@ class R {
 
     this._state = RESOLVED;
     this._value = value;
-    this._queue.forEach(this._handler.bind(this));
-    this._queue = null;
+    this._resolutionQueue.forEach(this._resolutionHandler.bind(this));
+    this._resolutionQueue = null; // This ensures the queue cannot be re-populated.
   }
 
+  /**
+   * Transitions the state from PENDING TO REJECTED, assigns `_reason`, and runs the rejection handler across the rejection queue.
+   *
+   * @param {Any} reason
+   */
   _reject(reason) {
     if (this._state !== PENDING) {
       return;
     }
 
     this._state = REJECTED;
-    this._value = reason;
-    this._queue.forEach(this._handler.bind(this));
-    this._queue = null;
+    this._reason = reason;
+    this._rejectionQueue.forEach(this._rejectionHandler.bind(this));
+    // Run through the resolution handler & reject all promises.
+    this._resolutionQueue.forEach(
+      function (resolution) {
+        resolution.newPromise._reject(this._reason);
+      }.bind(this)
+    );
+    this._rejectionQueue = null; // This ensures the queue cannot be re-populated.
   }
 
-  _handler(queueitem) {
-    let queueresponse = queueitem.onFulfilled(this._value);
-    if (queueresponse && queueresponse instanceof R) {
+  /**
+   * Invokes the callback function either from the resolution queue, or directly from `then`.
+   *
+   * @param {{onFulfilled: function | R, newPromise: R}} queueItem Contains both the callback function & the `then` promise.
+   */
+  _resolutionHandler(queueItem) {
+    let queueResponse;
+    try {
+      queueResponse = queueItem.onFulfilled(this._value);
+    } catch (error) {
+      queueItem.newPromise._reject(error);
+    }
+    if (queueResponse && queueResponse instanceof R) {
       // If queue item is a promise, wait for promise to resolve before resolving the `then` that the queue item was attached to.
-      queueresponse.then(function (value) {
-        queueitem.newPromise._resolve(value);
-      });
+      queueResponse
+        .then(function (value) {
+          queueItem.newPromise._resolve(value);
+        })
+        .catch(function (error) {
+          queueItem.newPromise._reject(error);
+        });
     } else {
       // Otherwise, immediately resolve the `then` promise.
-      queueitem.newPromise._resolve(this._value);
+      queueItem.newPromise._resolve(this._value);
     }
   }
 
+  /**
+   * Invokes the callback function from the rejection queue, or directly from `catch`
+   *
+   * @param {{onError: function, newPromise: R}} queueItem Contains both the callback function & the `then` promise.
+   */
+  _rejectionHandler(queueItem) {
+    let queueResponse = queueItem.onError(this._reason);
+    if (queueResponse && queueResponse instanceof R) {
+      queueResponse
+        .then(function (reason) {
+          queueItem.newPromise._reject(reason);
+        })
+        .catch(function (error) {
+          queueItem.newPromise._reject(reason);
+        });
+    } else {
+      queueItem.newPromise._reject(this.reason);
+    }
+  }
+
+  /**
+   * Takes a callback and pushes it to a resolution queue if the state is pending,
+   * but will immediately invokes the callback if the state is not pending.
+   * Returns a Promise (that can be resolved/rejected based on the callback).
+   *
+   * @param {Function|R} onFulfilled
+   * @param {*} onRejected
+   * @returns {R}
+   */
   then(onFulfilled, onRejected) {
+    let newPromise = new R(function (res, rej) {});
+
     if (this._state === RESOLVED) {
-      onFulfilled(this._value);
+      this._resolutionHandler({
+        onFulfilled,
+        newPromise
+      });
       return;
     }
 
-    let newPromise = new R(function (res, rej) {});
+    if (this._state === REJECTED) {
+      newPromise._reject(this._reason);
+    }
 
-    this._queue.push({
+    this._resolutionQueue.push({
       onFulfilled,
-      onRejected,
       newPromise
     });
     return newPromise;
   }
 
-  catch(reason) {}
+  /**
+   * Takes a callback function and pushes it into the rejection queue if the state is pending,
+   * but will immediately invokes the callback if the state is not pending.
+   *
+   * @param {Function} onError
+   */
+  catch(onError) {
+    let newPromise = new R(function (res, rej) {});
+    if (this._state === REJECTED) {
+      this._rejectionHandler({
+        onError,
+        newPromise
+      });
+      return;
+    }
+
+    this._rejectionQueue.push({
+      onError,
+      newPromise
+    });
+  }
 }
-
-// let response;
-
-// let promise = new R(function (resolve) {
-//   setTimeout(function () {
-//     resolve();
-//   }, 500);
-// });
-
-// promise
-//   .then(function () {
-//     return new R(function (resolve) {
-//       setTimeout(function () {
-//         resolve('hey there');
-//       }, 1000);
-//     });
-//   })
-//   .then(function (res) {
-//     response = res;
-//     console.log(response);
-//   });
 
 module.exports = R;
